856bcd269fd5842124cc53eaab2a7db572c96065
michael <made_in_51@hotmail.com>
2020-11-23 11:05:55 +0100

    maj page article/marque ajout class App

--- src/tools/App.php
+++ src/tools/App.php
@@ -3,6 +3,62 @@ declare(strict_types=1);
 
 namespace Oc\Tools;
 
+use Oc\Tools\DbConnect;
+
 class App
 {
+    private $id_article;
+    private $titre;
+    private $contenu;
+    private $extrait;
+    private $image;
+    private $prix;
+
+    public function __construct()
+    {
+        $this->id_article = ;
+        
+    }
+
+    public function getId()
+    {
+        return $this->id_article;
+    }
+
+    public function getTitle()
+    {
+        return $this->titre;
+    }
+
+    public function setTitle(string $titre)
+    {
+        $this->titre = $titre;
+
+        return $this->titre;
+    }
+
+    public function getContent()
+    {
+        return $this->contenu;
+    }
+
+    public function setContent(string $contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getExtrait()
+    {
+        return $this->extrait;
+    }
+
+    public function setExtrait(string $extrait)
+    {
+        $this->extrait = $extrait;
+
+        return $this;
+    }
+
 }
